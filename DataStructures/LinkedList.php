68673314868f594abfd22a9e664dd110d6f50315
Sunwarul Islam <sanoarulslm@gmail.com>
2021-02-22 14:12:41 +0600

    search, delete, deleteFirst, deleteLast functionalities implemented.

--- DataStructures/LinkedList.php
+++ DataStructures/LinkedList.php
@@ -34,8 +34,150 @@ class LinkedList
         $this->total_nodes++;
         return TRUE;
     }
+    public function insertAtFirst($data = NULL)
+    {
+        $new_node = new ListNode($data);
+        if ($this->first_node === NULL) {
+            $this->first_node = &$new_node;
+        } else {
+            $current_first_node = $this->first_node;
+            $this->first_node = &$new_node;
+            $new_node->next = $current_first_node;
+        }
+        $this->total_nodes++;
+        return true;
+    }
 
-    public function display()
+    /**
+     * Insert after a specific Node in linked list.
+     *
+     * @param string $data
+     * @param string $query
+     * @return void
+     */
+    public function insertAfter(string $data = NULL, string $query)
+    {
+        if ($this->total_nodes) {
+            $next_node = NULL;
+            $new_node = new ListNode($data);
+            $current_node = $this->first_node;
+
+            while ($current_node !== NULL) {
+
+                if ($current_node->data === $query) {
+                    if ($next_node !== NULL) {
+                        $new_node->next = &$next_node;
+                    }
+                    $current_node->next = $new_node;
+                    $this->total_nodes++;
+                    break;
+                }
+
+                $current_node = $current_node->next;
+                $next_node = $current_node->next;
+            }
+        }
+    }
+
+    /**
+     * Searching functions inside LinkedList
+     *
+     * @param string $data
+     * @return mixed
+     */
+    public function search(string $data = NULL)
+    {
+        if ($this->total_nodes) {
+            $current_node = $this->first_node;
+            while ($current_node !== NULL) {
+                if ($current_node->data === $data) {
+                    // print_r($current_node);
+                    return $current_node;
+                }
+                $current_node = $current_node->next;
+            }
+            return false;
+        }
+    }
+
+    /** 
+     * Delete first node    
+     *
+     * @return bool
+     */
+    public function deleteFirstNode(): bool
+    {
+        if ($this->first_node !== NULL) {
+            if ($this->first_node->next !== NULL) {
+                $this->first_node = $this->first_node->next;
+            } else {
+                $this->first_node = NULL;
+            }
+            $this->total_nodes--;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Delete last node
+     *
+     * @return boolean
+     */
+    public function deleteLastNode(): bool
+    {
+        if ($this->first_node !== NULL) {
+            $current_node = $this->first_node;
+            while ($current_node->next !== NULL) {
+                $previousNode = $current_node;
+                $current_node = $current_node->next;
+            }
+            $previousNode->next = NULL;
+            $this->total_nodes--;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Search and delete
+     *
+     * @param string $query
+     */
+    public function delete(string $query = NULL)
+    {
+        if ($this->first_node) {
+            $previous = NULL;
+            $currentNode = $this->first_node;
+
+            while ($currentNode !== NULL) {
+                if ($currentNode->data === $query) {
+                    if ($currentNode->next === NULL) {
+                        $previous->next = NULL;
+                    } else {
+                        if (isset($previous)) {
+                            $previous->next = $currentNode->next;
+                        } else {
+                            $this->deleteFirstNode();
+                            break;
+                        }
+                    }
+
+                    $this->total_nodes--;
+                    break;
+                }
+                $previous = $currentNode;
+                $currentNode = $currentNode->next;
+            }
+        }
+    }
+
+    /**
+     * Display linked-list nodes
+     *
+     * @return void
+     */
+    public function display(): void
     {
         echo "Total items = {$this->total_nodes}" . PHP_EOL;
         $current_node = $this->first_node;
@@ -51,9 +193,20 @@ $linked_list->insert("One");
 $linked_list->insert("Two");
 $linked_list->insert("Three");
 
+// $linked_list->insertAtFirst("Zero");
+
 $linked_list->display();
 
-/* Total items = 3
-One
-Two
-Three */
+$linked_list->delete('One');
+$linked_list->display();
+
+// $linked_list->deleteLastNode();
+// $linked_list->display();
+
+// $linked_list->deleteFirstNode();
+// $linked_list->display();
+
+// $linked_list->insertAfter("Four", "Three");
+// $linked_list->display();
+
+// $linked_list->search("Zero");
