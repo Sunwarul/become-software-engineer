cba3b4e3e5ef8e15c9aa37890efd0cea419d8847
Sunwarul Islam <sanoarulslm@gmail.com>
2021-03-03 19:30:14 +0600

    php8.0-intl installed and wrote a program that detect name of any unicode character

---
+++ PHP/string/name_of_an_unicode_character.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Install the php8.0-intl extension before running this program 
+ * sudo apt-get install php8.0-intl
+ */
+
+var_dump(IntlChar::charName("."));
+var_dump(IntlChar::charName("🏩"));
+var_dump(IntlChar::charName(".", IntlChar::UNICODE_CHAR_NAME));
+var_dump(IntlChar::charName("\u{2603}"));
+var_dump(IntlChar::charName("\u{0000}"));
+
+echo "\u{2603}" . PHP_EOL;
