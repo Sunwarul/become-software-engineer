cccd131e3a503f4ea933d9f42e4bbe2436bbbf84
Sunwarul Islam <sanoarulslm@gmail.com>
2021-03-17 21:46:02 +0600

    convert to uppercase

---
+++ OtherProgrammingLanguages/Java/BasicJavaProgramming/src/StringUpperCase.java
@@ -0,0 +1,10 @@
+/**
+ * Convert to upperCase by string method
+ */
+public class StringUpperCase {
+    public static void main(String[] args) {
+        String randomText = "Hello wOrld";
+        System.out.println(randomText.toUpperCase());
+        // HELLO WORLD
+    }
+}
