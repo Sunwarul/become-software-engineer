af8cdb8926c1c535de7b37e9dab4866f3e396078
Sunwarul Islam <sanoarulslm@gmail.com>
2021-03-17 21:50:55 +0600

    convert to lowercase

---
+++ OtherProgrammingLanguages/Java/BasicJavaProgramming/src/StringLowerCase.java
@@ -0,0 +1,7 @@
+public class StringLowerCase {
+    public static void main(String[] args) {
+        String randomText = "Hello wOrld";
+        System.out.println(randomText.toLowerCase());
+        // hello world
+    }
+}
