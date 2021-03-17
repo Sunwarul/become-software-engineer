e1f681b0fc5c0f94c0233d4629cfb36c7707e1fa
Sunwarul Islam <sanoarulslm@gmail.com>
2021-03-17 21:36:00 +0600

    Type cating added

---
+++ OtherProgrammingLanguages/Java/BasicJavaProgramming/src/TypeCast.java
@@ -0,0 +1,7 @@
+public class TypeCast {
+    public static void main(String[] args) {
+        double previousValue = 2.5500D;
+        int afterCasted = (int) previousValue;
+        System.out.println(afterCasted);
+    }
+}
