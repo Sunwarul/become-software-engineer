47cf233173433d6abcbfac9451545189719bdd6c
Sunwarul Islam <sanoarulslm@gmail.com>
2021-03-27 14:57:30 +0600

    java environment updated.

---
+++ OtherProgrammingLanguages/Java/BasicJavaProgramming/src/DataStructures/Queue.java
@@ -0,0 +1,7 @@
+package DataStructures;
+
+public class Queue {
+    public static void main(String[] args) {
+        System.out.println("Queue");
+    }
+}
---
+++ OtherProgrammingLanguages/Java/BasicJavaProgramming/src/DataStructures/Stack.java
@@ -0,0 +1,7 @@
+package DataStructures;
+
+public class Stack {
+    public static void main(String[] args) {
+        System.out.println("Stack");
+    }
+}
