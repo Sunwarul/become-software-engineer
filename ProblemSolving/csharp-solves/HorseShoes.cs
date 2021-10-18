456c751e9165c81552d24b195b5f9bb03c08b136
Sunwarul Islam <sanoarulslm@gmail.com>
2021-10-19 01:43:54 +0600

    HorseShoes CF accepted.

--- ProblemSolving/csharp-solves/HorseShoes.cs
+++ ProblemSolving/csharp-solves/HorseShoes.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Linq;
 public class HorseShoes
 {
     public static void Main(string[] args)
     {
-        Console.Write("Horse Shoes");
+        string inputLine = Console.ReadLine();
+        int[] horseShoes = inputLine.Split(" ").Select(int.Parse).ToArray();
+        int[] uniqueHorseShoes = horseShoes.Distinct().ToArray();
+
+        Console.WriteLine(horseShoes.Length - uniqueHorseShoes.Length);
     }
 }
