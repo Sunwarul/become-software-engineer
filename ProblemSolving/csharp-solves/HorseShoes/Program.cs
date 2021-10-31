91dfc997df4a30c321a687168b16b23b47cad351
Sunwarul Islam <sanoarulslm@gmail.com>
2021-10-31 23:48:37 +0600

    array reading example

--- ProblemSolving/csharp-solves/HorseShoes/Program.cs
+++ ProblemSolving/csharp-solves/HorseShoes/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, horse shoes!");
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter size:");
+        int size = 2;
+        int.TryParse(Console.ReadLine(), out size);
+        int[] arr = new int[size];
+
+        Console.WriteLine("Enter values: ");
+        for (int i = 0; i < arr.Count(); i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Results: ");
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item * 2);
+        }        
+    }
+}
