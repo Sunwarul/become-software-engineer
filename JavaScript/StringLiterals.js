ec4530352f313584e09ea5da999a75709a8f1d89
Sunwarul Islam <sanoarulslm@gmail.com>
2022-07-20 23:56:00 +0600

    string interpolation added.

---
+++ JavaScript/StringLiterals.js
@@ -0,0 +1,12 @@
+const str = `hello world`
+
+console.log(str);
+console.log(`Using variables ${str}`);
+console.log(`Expression: ${200 * 33}`);
+console.log(`Using single' and Double" quotations`);
+
+console.log(`
+This 
+is a multiline 
+string
+`);
