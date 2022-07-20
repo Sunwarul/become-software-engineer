ff9bfe63bb92c28e4d234fcf14950e139449920c
Sunwarul Islam <sanoarulslm@gmail.com>
2022-07-21 00:16:03 +0600

    numbers snippets

---
+++ JavaScript/Numbers.js
@@ -0,0 +1,15 @@
+/**
+ * All JS numbers are 64-bit floating numbers
+ * Resource: https://www.w3schools.com/js/js_numbers.asp
+ */
+
+/**
+ Important methods:
+
+- isNaN()
+- Infinity
+- -Infinity
+- typeof(Infinity) and typeof(NaN) is number
+- toString(base)
+
+*/
